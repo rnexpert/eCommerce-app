c1cd57886e066dedd5e6d99a0064f144b50cf56b
rnexpert <jamttdev657@proton.me>
2021-01-29 19:43:03 -0500

    Redux to delete an item from the cart and redux for orders

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { View, Text, FlatList, StyleSheet, Button } from "react-native";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import CartItem from "../../components/shop/CartItem";
 import Colors from "../../constants/colors";
+import * as cartActions from "../../store/actions/cart";
+import * as ordersActions from "../../store/actions/orders";
 
 const CartScreen = (props) => {
   const cartTotalAmount = useSelector((state) => state.cart.totalAmount);
@@ -18,8 +20,10 @@ const CartScreen = (props) => {
         sum: state.cart.items[key].sum,
       });
     }
-    return transformedCardItem;
+    return transformedCardItem.sort((a, b) => a.productId > b.productId ? 1 : -1);
   });
+
+  const dispatch = useDispatch();
 
   return (
     <View style={styles.screen}>
@@ -32,6 +36,9 @@ const CartScreen = (props) => {
           title="Order Now"
           color={Colors.thirdColor}
           disabled={cartItems.length === 0}
+          onPress={()=>{
+            dispatch(ordersActions.addOrder(cartItems, cartTotalAmount));
+          }}
         />
       </View>
       <FlatList
@@ -42,7 +49,9 @@ const CartScreen = (props) => {
             quantity={itemData.item.quantity}
             title={itemData.item.productTitle}
             amount={itemData.item.sum}
-            onRemove={() => {}}
+            onRemove={() => {
+              dispatch(cartActions.removeFromCart(itemData.item.productId));
+            }}
           />
         )}
       />
--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,35 +1,57 @@
-import { ADD_TO_CART } from "../actions/cart";
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../actions/cart";
 import CartItem from "../../models/cart-item";
 
 const initialState = {
-    items: {},
-    totalAmount: 0
+  items: {},
+  totalAmount: 0,
 };
 
-export default(state = initialState, action) => {
-    switch(action.type) {
-        case ADD_TO_CART:
-            const addedProduct = action.product;
-            const prodPrice = addedProduct.price;
-            const prodTitle = addedProduct.title;
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      const addedProduct = action.product;
+      const prodPrice = addedProduct.price;
+      const prodTitle = addedProduct.title;
 
-            let updatedOrNewItem;
+      let updatedOrNewItem;
 
-            if(state.items[addedProduct.id]) {
-                updatedOrNewItem = new CartItem(
-                    state.items[addedProduct.id].quantity + 1,
-                    prodPrice,
-                    prodTitle,
-                    state.items[addedProduct.id].sum + prodPrice
-                );
-            } else {
-                updatedOrNewItem = new CartItem(1, prodPrice, prodTitle, prodPrice);
-            }
-            return {
-                ...state,
-                items: { ...state.items, [addedProduct.id]: updatedOrNewItem},
-                totalAmount: state.totalAmount + prodPrice
-            }
-    }
-    return state;
+      if (state.items[addedProduct.id]) {
+        updatedOrNewItem = new CartItem(
+          state.items[addedProduct.id].quantity + 1,
+          prodPrice,
+          prodTitle,
+          state.items[addedProduct.id].sum + prodPrice
+        );
+      } else {
+        updatedOrNewItem = new CartItem(1, prodPrice, prodTitle, prodPrice);
+      }
+      return {
+        ...state,
+        items: { ...state.items, [addedProduct.id]: updatedOrNewItem },
+        totalAmount: state.totalAmount + prodPrice,
+      };
+
+    case REMOVE_FROM_CART:
+      const selectedCartItem = state.items[action.pid];
+      const currentQty = selectedCartItem.quantity;
+      let updatedCartItem;
+      if (currentQty > 1) {
+        updatedCartItem = new CartItem(
+          selectedCartItem.quantity - 1,
+          selectedCartItem.productPrice,
+          selectedCartItem.productTitle,
+          selectedCartItem.sum
+        );
+        updatedCartItem = { ...state.items, [action.pid]: updatedCartItem };
+      } else {
+        updatedCartItem = { ...state.items };
+        delete updatedCartItem[action.pid];
+      }
+      return { 
+          ...state,
+          items: updatedCartItem,
+          totalAmount: state.totalAmount - selectedCartItem.productPrice
+      };
+  }
+  return state;
 };
